12a3eb87212b1714b8d5f5a9f1edc49d83f97d61
david.birdsong <david.birdsong@netlify.com>
2025-09-02 14:08:20 -0700

    don't hide files

---
+++ .config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,15 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+        opts.close_if_last_window = true
+        opts.enable_git_status = true
+        opts.filesystem = opts.filesystem or {}
+        opts.filesystem.filtered_items = vim.tbl_deep_extend("force", opts.filesystem.filtered_items or {}, {
+            visible = true,
+            never_show = { ".git" },
+            --hide_dotfiles = false,
+            -- hide_gitignored = false,
+            --
+        })
+    end,
+}
